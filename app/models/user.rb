275bc93c48916bd7f69f5f2d60eb84cca4199113
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-20 21:48:01 +0630

    Fix the linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- has_many :food
- has_many :recipe
+  has_many :food
+  has_many :recipe
 end
