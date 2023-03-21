7b7d6880df5d83e536d29a02067d3ed58bd0e5e3
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-21 21:33:50 +0630

    Fix the linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,7 @@ class FoodsController < ApplicationController
     @food = Food.all
   end
 
-  def new 
+  def new
     @food = Food.new
     respond_to do |format|
       format.html { render :new, locals: { food: @food } }
@@ -11,25 +11,24 @@ class FoodsController < ApplicationController
   end
 
   def create
-  @food = Food.new(food_params.merge(user_id: current_user.id))
-  respond_to do |format|
-    format.html do
-      if @food.save!
-        flash[:success] = "food saved successfully"
-        redirect_to foods_url
-      else 
-        flash.now[:error] = "Error: Food could not be saved"
-        render :new, locals: { food: @food }
+    @food = Food.new(food_params.merge(user_id: current_user.id))
+    respond_to do |format|
+      format.html do
+        if @food.save!
+          flash[:success] = 'food saved successfully'
+          redirect_to foods_url
+        else
+          flash.now[:error] = 'Error: Food could not be saved'
+          render :new, locals: { food: @food }
+        end
       end
     end
   end
-end
 
- 
+
   private
 
   def food_params
-  params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
-
 end
