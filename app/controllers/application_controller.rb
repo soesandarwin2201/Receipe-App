1c2c1e35ed1c4a4c1ca3d41a2c85212047c1bb2a
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-22 16:15:42 +0630

    add the validation for the foods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :current_password)
     end
   end
+
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to root_url, alert: exception.message
+  end
 end
