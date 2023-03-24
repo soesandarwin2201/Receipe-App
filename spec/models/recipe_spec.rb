f41ef2d58a08d60e78f864a26be1ccc7ecdfcce6
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-24 19:43:35 +0630

    create the test

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do 
+ describe  'Recipe model' do
+ let(:user) { User.create(name: 'soe', email: 'soesandarwin2201@gmail.com', password: '22010510') }
+ let(:recipe)  { Recipe.new(user: user, name: 'pizza', preparation_time: '3', cooking_time: '2', description: 'it is good', public: true)}
+
+   before{ recipe.save } 
+
+   it 'check the name is not blank' do 
+    recipe.name = 'pizza'
+    expect(recipe).to be_valid  
+   end
+
+   it 'check the preparation_time is not blank' do 
+    recipe.preparation_time = '3' 
+    expect(recipe).to be_valid  
+   end
+
+   it 'check the cooking is not blank' do 
+    recipe.cooking_time = '2'
+    expect(recipe).to be_valid  
+   end
+
+   it 'check the description is not blank' do 
+    recipe.description = 'it is good'
+    expect(recipe).to be_valid  
+   end
+   end
+end
