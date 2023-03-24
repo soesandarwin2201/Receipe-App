5da0063bbacdc8a16384d317c8d3fe92f0dfa125
Alabi12 <wolo22001@gmail.com>
2023-03-24 11:43:19 +0000

    Add shopping list and perform linter errors

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,10 +1,10 @@
 class ShoppingListController < ApplicationController
-  def index 
-      # to show the recipe of the current user 
-      @recipes = Recipe.where(user_id: current_user.id)
-      # get the foodlist of the recipe 
-      @recipe_foods = RecipeFood.where(recipe_id: @recipes.ids)
-      # general food 
-      @foods = Food.where(id: @recipe_foods.pluck(:food_id))
-    end
+  def index
+    # to show the recipe of the current user
+    @recipes = Recipe.where(user_id: current_user.id)
+    # get the foodlist of the recipe
+    @recipe_foods = RecipeFood.where(recipe_id: @recipes.ids)
+    # general food
+    @foods = Food.where(id: @recipe_foods.pluck(:food_id))
+  end
 end
